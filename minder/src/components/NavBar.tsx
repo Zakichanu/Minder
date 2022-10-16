37553deb2c4ceee31f505c2e80b424935d70bee3
Zakaria Tobbal <tzakaria@hotmail.fr>
2022-10-16 21:39:40 +0200

    Début de responsive de l'AppBar

--- minder/src/components/NavBar.tsx
+++ minder/src/components/NavBar.tsx
@@ -1,9 +1,28 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import React from 'react';
 import Toolbar from '@mui/material/Toolbar';
 
 export default function ButtonAppBar() {
+
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+  // Fermeture du menu 
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  // Ouverture du menu
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -15,11 +34,48 @@ export default function ButtonAppBar() {
               height: 50,
               width: 150,
             }}
-            alt="The house from the offer."
+            alt="Minder"
             src="../logo-complete@2x.png"
             onClick={() => window.location.href = "/"}
           />
-          <Button color="inherit" onClick={() => window.location.href = "/SignupSC"}>Espace partenaires</Button>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            <Button color="inherit" onClick={() => window.location.href = "/SignupSC"}>Espace partenaires</Button>
+          </Box>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+          </Box>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{
+              display: { xs: 'block', md: 'none' },
+            }}
+          >
+            <MenuItem>
+              <Button color="inherit" onClick={() => window.location.href = "/SignupSC"}>Espace partenaires</Button>
+            </MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
     </Box>
