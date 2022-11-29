c0024d78a45cd81f6bb7b1de2a9ca02f9b81fd8c
Zakaria Tobbal <tzakaria@hotmail.fr>
2022-11-29 16:58:25 +0100

    making home_page into routes because the subject changes

--- minder_mobile/lib/screens/routes.dart
+++ minder_mobile/lib/screens/routes.dart
@@ -1,15 +1,15 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key, required this.title}) : super(key: key);
+class RoutesPage extends StatefulWidget {
+  const RoutesPage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<RoutesPage> createState() => _RoutesState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _RoutesState extends State<RoutesPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
