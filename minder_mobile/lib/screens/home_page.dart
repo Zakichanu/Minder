c5070f23cb0a9ca81469782290e40e2668eb8616
Zakaria Tobbal <tzakaria@hotmail.fr>
2022-11-29 16:42:55 +0100

    update navbar pages : reservation;sports+evenement;homepage;sportcenter;settings

--- minder_mobile/lib/screens/home_page.dart
+++ minder_mobile/lib/screens/home_page.dart
@@ -16,9 +16,11 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar: CurvedNavigationBar(
         backgroundColor: const Color.fromRGBO(144, 150, 251, 1),
         items: const <Widget>[
-          Icon(Icons.list, size: 30),
+          Icon(Icons.event, size: 30),
+          Icon(Icons.sports, size: 30),
           Icon(Icons.home, size: 30),
-          Icon(Icons.person, size: 30),
+          Icon(Icons.business_outlined, size: 30),
+          Icon(Icons.settings, size: 30),
         ],
         onTap: (index) {
           //Handle button tap
